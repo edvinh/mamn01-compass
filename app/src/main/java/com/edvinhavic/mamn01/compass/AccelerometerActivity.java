1c3b9d1cbc933791003236f0c1b5be74892cc622
Edvin Havic <edvinhavic@gmail.com>
2017-04-07 16:44:48 +0200

    Updated accelerometer activity

--- app/src/main/java/com/edvinhavic/mamn01/compass/AccelerometerActivity.java
+++ app/src/main/java/com/edvinhavic/mamn01/compass/AccelerometerActivity.java
@@ -2,12 +2,50 @@ package com.edvinhavic.mamn01.compass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.SensorEventListener;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.widget.TextView;
 
-public class AccelerometerActivity extends AppCompatActivity {
 
+public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private TextView mTextViewX, mTextViewY, mTextViewZ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer);
+
+        senSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+
+        mTextViewX = (TextView) findViewById(R.id.x_val);
+        mTextViewY = (TextView) findViewById(R.id.y_val);
+        mTextViewZ = (TextView) findViewById(R.id.z_val);
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+            mTextViewX.setText(x + "");
+            mTextViewY.setText(y + "");
+            mTextViewZ.setText(z + "");
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
